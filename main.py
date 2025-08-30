eac8559dcd91ea23a4236be73945b45272f80edf
nazir17 <naziruddin@tothebyte.com>
2025-08-30 17:00:53 +0530

    service added

--- main.py
+++ main.py
@@ -1,46 +1,33 @@
-from fastapi import FastAPI
-import file_storage as fstore
-import memory_storage as mem
+from fastapi import FastAPI, HTTPException
+import storage_service as service
 
 app = FastAPI()
 
 @app.get("/api/users")
 def get_users():
-    return mem.get_users()
+    return service.get_users()
 
 @app.post("/api/users")
-def create_user(user: mem.UserBase):
-    return mem.create_user(user)
+def create_user(user: service.UserBase):
+    return service.create_user(user)
 
 @app.get("/api/users/{user_id}")
 def get_user(user_id: int):
-    return mem.get_user(user_id)
+    user = service.get_user(user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
 
 @app.put("/api/users/{user_id}")
-def update_user(user_id: int, user: mem.UserBase):
-    return mem.update_user(user_id, user)
+def update_user(user_id: int, user: service.UserBase):
+    updated_user = service.update_user(user_id, user)
+    if not updated_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return updated_user
 
 @app.delete("/api/users/{user_id}")
 def delete_user(user_id: int):
-    return mem.delete_user(user_id)
-
-# endpoints of storage
-@app.get("/api/users-files")
-def get_users_file():
-    return fstore.get_users_file()
-
-@app.post("/api/users-files")
-def create_user_file(user: fstore.UserBase):
-    return fstore.create_user_file(user)
-
-@app.get("/api/users-files/{user_id}")
-def get_user_file(user_id: int):
-    return fstore.get_user_file(user_id)
-
-@app.put("/api/users-files/{user_id}")
-def update_user_file(user_id: int, user: fstore.UserBase):
-    return fstore.update_user_file(user_id, user)
-
-@app.delete("/api/users-files/{user_id}")
-def delete_user_file(user_id: int):
-    return fstore.delete_user_file(user_id)
+    deleted_user = service.delete_user(user_id)
+    if not deleted_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return deleted_user
